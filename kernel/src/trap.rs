e1d3417b4d377e99cf1bb5b4b4f51bf98f2c074b
algon-320 <algon.0320@gmail.com>
2020-08-23 14:41:57 +0900

    fix indent

--- kernel/src/trap.rs
+++ kernel/src/trap.rs
@@ -115,31 +115,31 @@ global_asm! {r#"
 # vectors.S sends all traps here.
 .globl alltraps
 alltraps:
-  # Build trap frame.
-  pushl %ds
-  pushl %es
-  pushl %fs
-  pushl %gs
-  pushal
+    # Build trap frame.
+    pushl %ds
+    pushl %es
+    pushl %fs
+    pushl %gs
+    pushal
 
-  # Set up data segments.
-  movw $(2<<3), %ax  # SEG_KDATA<<3
-  movw %ax, %ds
-  movw %ax, %es
+    # Set up data segments.
+    movw $(2<<3), %ax  # SEG_KDATA<<3
+    movw %ax, %ds
+    movw %ax, %es
 
-  # Call trap(tf), where tf=%esp
-  pushl %esp
-  call trap
-  addl $4, %esp
+    # Call trap(tf), where tf=%esp
+    pushl %esp
+    call trap
+    addl $4, %esp
 
 # Return falls through to trapret...
 .globl trapret
 trapret:
-  popal
-  popl %gs
-  popl %fs
-  popl %es
-  popl %ds
-  addl $0x8, %esp  # trapno and errcode
-  iret
+    popal
+    popl %gs
+    popl %fs
+    popl %es
+    popl %ds
+    addl $0x8, %esp  # trapno and errcode
+    iret
 "#}
