5c2ffdac525b3a648ce50fd5b845e3cde1a78a5f
algon-320 <algon.0320@gmail.com>
2020-08-28 14:11:44 +0900

    impl inode

--- kernel/src/fs.rs
+++ kernel/src/fs.rs
@@ -68,9 +68,70 @@ pub mod bcache {
     }
 }
 
+const N_DIRECT: usize = 12;
+const N_INDIRECT: usize = BLOCK_SIZE / core::mem::size_of::<u32>();
+
+// Directory is a file containing a sequence of dirent structures.
+const DIR_SIZE: usize = 14;
+
 pub mod inode {
+    use crate::lock::sleep::SleepMutex;
+    use crate::lock::spin::SpinMutex;
+    use crate::proc::my_proc;
+    use core::sync::atomic::{AtomicUsize, Ordering};
+    use utils::prelude::*;
+
+    const ROOT_DEV: usize = 1;
+    const ROOT_INO: usize = 1;
+
+    /// maximum number of active i-nodes
+    const N_INODE: usize = 50;
+    static ICACHE: SpinMutex<[Inode; N_INODE]> = SpinMutex::new("icache", [Inode::zero(); N_INODE]);
+
     /// in-memory copy of an inode
-    pub struct Inode {}
+    pub struct Inode {
+        dev: usize,           // Device number
+        inum: usize,          // Inode number
+        ref_cnt: AtomicUsize, // Reference count
+        body: SleepMutex<InodeBody>,
+    }
+    impl Inode {
+        pub const fn zero() -> Self {
+            Self {
+                dev: 0,
+                inum: 0,
+                ref_cnt: AtomicUsize::new(0),
+                body: SleepMutex::new("inode", InodeBody::zero()),
+            }
+        }
+        pub fn get_ref(&self) -> InodeRef {
+            self.ref_cnt.fetch_add(1, Ordering::SeqCst);
+            InodeRef { ptr: self }
+        }
+    }
+    pub struct InodeBody {
+        valid: bool,
+        type_: u16,
+        major: u16,
+        minor: u16,
+        nlink: u16,
+        size: usize,
+        addrs: [u32; super::N_DIRECT + 1],
+    }
+    impl InodeBody {
+        pub const fn zero() -> Self {
+            Self {
+                valid: false,
+                type_: 0,
+                major: 0,
+                minor: 0,
+                nlink: 0,
+                size: 0,
+                addrs: [0; super::N_DIRECT + 1],
+            }
+        }
+    }
+
     pub struct InodeRef {
         ptr: *const Inode,
     }
@@ -80,9 +141,58 @@ pub mod inode {
                 ptr: core::ptr::null(),
             }
         }
+        pub fn dup(&self) -> Self {
+            let _guard = ICACHE.lock();
+            assert!(!self.ptr.is_null(), "dup on dangling InodeRef");
+            unsafe { (*self.ptr).ref_cnt.fetch_add(1, Ordering::SeqCst) };
+            Self { ptr: self.ptr }
+        }
+    }
+    impl Drop for InodeRef {
+        fn drop(&mut self) {
+            if !self.ptr.is_null() {
+                let _guard = ICACHE.lock();
+                log!("InodeRef drop");
+                unsafe { (*self.ptr).ref_cnt.fetch_sub(1, Ordering::SeqCst) };
+            }
+        }
     }
 
-    pub fn from_name(path: &str) -> InodeRef {
+    fn init() {
         todo!()
+    }
+
+    fn get(dev: usize, inum: usize) -> InodeRef {
+        let mut icache = ICACHE.lock();
+
+        let mut empty: *mut Inode = core::ptr::null_mut();
+        for ip in icache.iter_mut() {
+            if ip.ref_cnt.load(Ordering::SeqCst) > 0 && ip.dev == dev && ip.inum == inum {
+                return ip.get_ref(); // ref_cnt += 1
+            }
+            if empty.is_null() && ip.ref_cnt.load(Ordering::SeqCst) == 0 {
+                empty = ip;
+            }
+        }
+        assert!(!empty.is_null(), "inode::get: no inodes");
+
+        let ip = unsafe { &mut *empty };
+        ip.dev = dev;
+        ip.inum = inum;
+        ip.ref_cnt = AtomicUsize::new(0);
+        ip.body.lock().valid = false;
+        ip.get_ref() // ref_cnt -> 1
+    }
+
+    fn name_x(path: &str, name_iparent: bool, name: &mut [u8]) -> InodeRef {
+        let ip = match path {
+            "/" => get(ROOT_DEV, ROOT_INO),
+            _ => unsafe { (*my_proc()).cwd.dup() },
+        };
+        todo!()
+    }
+    pub fn from_name(path: &str) -> InodeRef {
+        let mut name = [0; super::DIR_SIZE];
+        name_x(path, true, &mut name)
     }
 }
