804ef44603c4355a1c39d504b7972174ba30a1e2
algon-320 <algon.0320@gmail.com>
2020-09-29 20:32:48 +0900

    update ide.rs

--- kernel/src/fs/ide.rs
+++ kernel/src/fs/ide.rs
@@ -1,4 +1,5 @@
 use super::bcache::BufRef;
+use super::BLK_SIZE;
 use crate::ioapic;
 use crate::lock::spin::SpinMutex;
 use crate::proc;
@@ -6,6 +7,25 @@ use crate::trap;
 use alloc::collections::VecDeque;
 use lazy_static::lazy_static;
 use utils::x86;
+
+const SECTOR_SIZE: usize = 512;
+
+const IDE_BSY: u8 = 0x80;
+const IDE_DRDY: u8 = 0x40;
+const IDE_DF: u8 = 0x20;
+const IDE_ERR: u8 = 0x01;
+
+const IDE_CMD_READ: u8 = 0x20;
+const IDE_CMD_WRITE: u8 = 0x30;
+const IDE_CMD_RDMUL: u8 = 0xC4;
+const IDE_CMD_WRMUL: u8 = 0xC5;
+
+const PORT_BASE: u16 = 0x1F0;
+
+static mut HAVE_DISK1: bool = false;
+fn have_disk1() -> bool {
+    unsafe { HAVE_DISK1 }
+}
 
 lazy_static! {
     static ref IDE_QUEUE: SpinMutex<IdeQueue> = SpinMutex::new("IDE_QUE", IdeQueue::new());
@@ -22,9 +42,7 @@ impl IdeQueue {
             running: false,
         }
     }
-    fn start(&mut self) {
-        todo!()
-    }
+
     pub fn append(&mut self, b: BufRef) {
         self.que.push_back(b);
         // Start disk if necessary.
@@ -32,22 +50,72 @@ impl IdeQueue {
             self.start();
         }
     }
+
     pub fn is_empty(&self) -> bool {
         self.que.is_empty()
     }
+
+    fn start(&mut self) {
+        let b = match self.que.front() {
+            Some(b) => b,
+            None => return,
+        };
+        let sector_per_block = BLK_SIZE / SECTOR_SIZE;
+        let sector = b.block_no as usize * sector_per_block;
+        let read_cmd = if sector_per_block == 1 {
+            IDE_CMD_READ
+        } else {
+            IDE_CMD_RDMUL
+        };
+        let write_cmd = if sector_per_block == 1 {
+            IDE_CMD_WRITE
+        } else {
+            IDE_CMD_WRMUL
+        };
+        assert!(sector_per_block <= 7, "IdeQueue::start");
+
+        wait(false);
+        x86::outb(0x3F6, 0); // generate interrupt
+        x86::outb(0x1F2, sector_per_block as u8); // number of sectors
+        x86::outb(0x1F3, (sector & 0xFF) as u8);
+        x86::outb(0x1F4, ((sector >> 8) & 0xFF) as u8);
+        x86::outb(0x1F5, ((sector >> 16) & 0xFF) as u8);
+        x86::outb(
+            0x1F6,
+            0xE0 | (((b.dev & 1) << 4) as u8) | ((sector >> 24) & 0x0F) as u8,
+        );
+        if b.dirty() {
+            x86::outb(0x1F7, write_cmd);
+            let data = b.data.lock();
+            x86::outsl(0x1F0, data.as_ptr() as *const u32, data.len() / 4);
+        } else {
+            x86::outb(0x1F0, read_cmd);
+        }
+
+        self.running = true;
+    }
+
+    fn notify_ready(&mut self) {
+        self.running = false;
+        let b = self.que.pop_front().unwrap();
+
+        // Read data if needed.
+        if b.dirty() && wait(true).is_some() {
+            let mut data = b.data.lock();
+            x86::insl(0x1F0, data.as_mut_ptr() as *mut u32, data.len() / 4);
+        }
+
+        // Wake process waitint for this buf.
+        b.set_valid();
+        b.clear_dirty();
+        todo!(); // wakeup(b);
+
+        // Start disk on next buf in queue.
+        if !self.is_empty() {
+            self.start();
+        }
+    }
 }
-
-static mut HAVE_DISK1: bool = false;
-fn have_disk1() -> bool {
-    unsafe { HAVE_DISK1 }
-}
-
-const IDE_BSY: u8 = 0x80;
-const IDE_DRDY: u8 = 0x40;
-const IDE_DF: u8 = 0x20;
-const IDE_ERR: u8 = 0x01;
-
-const PORT_BASE: u16 = 0x1F0;
 
 /// Wait for IDE disk to become ready.
 fn wait(check_err: bool) -> Option<()> {
@@ -114,11 +182,11 @@ pub fn write_to_disk(b: &BufRef) {
     }
 }
 
+/// Interrupt handler.
 #[no_mangle]
 pub extern "C" fn ide_intr() {
     let mut ide_que = IDE_QUEUE.lock();
-    if ide_que.is_empty() {
-        return;
+    if !ide_que.is_empty() {
+        ide_que.notify_ready();
     }
-    todo!();
 }
